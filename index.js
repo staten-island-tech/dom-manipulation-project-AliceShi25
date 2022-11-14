68deeeadb06b661c5fac2cee23255e10c6c78c6e
AliceShi25 <shialice2006@gmail.com>
2022-11-14 13:14:48 -0500

    f

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const DOMSelectors = {
   parent: document.getElementById("parent"),
+  // inputs: document.querySelector(".inputs"),
   inputs: document.querySelector(".inputs"),
-  input: document.querySelector(".input"),
   inputbox: document.querySelector(".inputbox"),
   input1: document.getElementById("input1"),
   input2: document.getElementById("input2"),
@@ -9,36 +9,15 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
 };
 
-// function colorChange(parent, text) {
-//   parent.style.backgroundColor = "red";
-//   text.style.color = "white";
-// }
-
 DOMSelectors.button.addEventListener("click", function () {
   let input1 = DOMSelectors.input1.value;
-  DOMSelectors.button.insertAdjacentHTML("afterend", `<p>${input1}</p>`);
+  let input2 = DOMSelectors.input2.value;
+  let url = DOMSelectors.url.value;
+  DOMSelectors.button.insertAdjacentHTML(
+    "afterend",
+    `<div id="die"><p>${input1} ${input2} ${url} </p></div>`
+  );
   DOMSelectors.input1.value = "";
+  DOMSelectors.input2.value = "";
+  DOMSelectors.url.value = "";
 });
-
-// DOMSelectors.button2.addEventListener("click", function () {
-//   let input = DOMSelectors.text.value;
-//   DOMSelectors.text.value = "";
-// });
-
-// DOMSelectors.button.insertAdjacentHTML("afterend", `<h1>"hi"</h1>`);
-
-// when click button, function in bkgtxt happens
-// nodelist
-// const qsa = (els) => document.querySelectorAll(els);
-// const allPoint = qsa(".point");
-// console.log(allPoint);
-
-// const allPointArray = Array.from(allPoint);
-// const oPoint = allPointArray.filter((node) => {
-//   if (node.classList[1] === "one") {
-//     return true;
-//   }
-//   return false;
-// });
-
-// console.log(oPoint);
