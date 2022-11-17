184ff9b2e5d1bbc5670f582529e25b1428c150bc
AliceShi25 <shialice2006@gmail.com>
2022-11-17 18:05:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
 };
 
-DOMSelectors.button.addEventListener("click", function () {
+DOMSelectors.button.addEventListener("click", function (event) {
   let name = DOMSelectors.name.value;
   let message = DOMSelectors.message.value;
   let url = DOMSelectors.url.value;
   DOMSelectors.button.insertAdjacentHTML(
     "afterend",
     `<div id="coard"> 
-    <p class="cardName>From: ${name}</p> 
-    <p class="cardImage><img src="${url}" alt="image"> </p> 
-    <p class="cardMessage">${message}</p> 
+    <p>From: ${name}</p> 
+    <p><img src="${url}" alt="image"> </p> 
+    <p>${message}</p> 
     <button class="cleary">Clear</button> </div>`
   );
   DOMSelectors.name.value = "";
