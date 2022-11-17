c1415941fbf0e66ee28f69c5dcc844cd2d9c848e
AliceShi25 <shialice2006@gmail.com>
2022-11-17 18:50:15 -0500

    sigh

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const DOMSelectors = {
   parent: document.getElementById("parent"),
+  formparent: document.querySelector(".formparent"),
+  form: document.querySelector(".form"),
   inputs: document.querySelector(".inputs"),
   inputbox: document.querySelector(".inputbox"),
   name: document.getElementById("name"),
   message: document.getElementById("message"),
   url: document.getElementById("url"),
-  button: document.getElementById("btn"),
+  button: document.querySelector(".btn"),
+  display: document.querySelector("display"),
+  remove: document.querySelectorAll(".remove"),
 };
 
 DOMSelectors.button.addEventListener("click", function (event) {
@@ -14,10 +18,10 @@ DOMSelectors.button.addEventListener("click", function (event) {
   let url = DOMSelectors.url.value;
   DOMSelectors.button.insertAdjacentHTML(
     "afterend",
-    `<div id="coard"> 
-    <p>From: ${name}</p> 
-    <p><img src="${url}" alt="image"> </p> 
-    <p>${message}</p> 
+    `<div id="coard">
+    <p>From: ${name}</p>
+    <p><img src="${url}" alt="image"> </p>
+    <p>${message}</p>
     <button class="cleary">Clear</button> </div>`
   );
   DOMSelectors.name.value = "";
