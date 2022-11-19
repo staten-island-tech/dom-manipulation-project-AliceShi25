6517f0bf53e099342baa157eb7b1b0aab5f9c3af
AliceShi25 <shialice2006@gmail.com>
2022-11-18 19:14:24 -0500

    er

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 const DOMSelectors = {
-  parent: document.getElementById("parent"),
   formparent: document.querySelector(".formparent"),
-  form: document.querySelector(".form"),
+  form: document.getElementById("form"),
+  forminputs: document.querySelector(".form-inputs"),
   inputs: document.querySelector(".inputs"),
-  inputbox: document.querySelector(".inputbox"),
+  text: document.querySelector(".text"),
+  textbox: document.querySelector(".textbox"),
   name: document.getElementById("name"),
   message: document.getElementById("message"),
-  url: document.getElementById("url"),
+  img: document.getElementById("img"),
   button: document.querySelector(".btn"),
-  display: document.querySelector("display"),
-  remove: document.querySelectorAll(".remove"),
+  display: document.getElementById("display"),
+  clear: document.querySelectorAll(".clear"),
 };
 
-DOMSelectors.button.addEventListener("click", function (event) {
-  let name = DOMSelectors.name.value;
-  let message = DOMSelectors.message.value;
-  let url = DOMSelectors.url.value;
-  DOMSelectors.button.insertAdjacentHTML(
-    "afterend",
-    `<div id="coard">
-    <p>From: ${name}</p>
-    <p><img src="${url}" alt="image"> </p>
-    <p>${message}</p>
-    <button class="cleary">Clear</button> </div>`
-  );
-  DOMSelectors.name.value = "";
-  DOMSelectors.message.value = "";
-  DOMSelectors.url.value = "";
+DOMSelectors.form.addEventListener("submit", function (a) {
+  a.preventDefault();
+  let card = {};
+  card.name = DOMSelectors.name.value;
+  card.img = DOMSelectors.img.value;
+  card.message = DOMSelectors.message.value;
+  type();
+  clear();
+  function type() {
+    DOMSelectors.button.insertAdjacentHTML(
+      `afterend`,
+      `<div class="coard">
+      <p>From: ${card.name}</p>
+      <p><img src= "${card.img}"/></p>
+      <p>${card.message}</p>
+       <button class="clear">Clear</button>
+  </div>`
+    );
+    DOMSelectors.name.value = "";
+    DOMSelectors.img.value = "";
+    DOMSelectors.message.value = "";
+  }
+  function clear() {
+    let clear = document.querySelectorAll(".clear");
+    clear.forEach((node) => {
+      node.addEventListener("click", function (a) {
+        a.target.parentElement.remove();
+      });
+    });
+  }
 });
